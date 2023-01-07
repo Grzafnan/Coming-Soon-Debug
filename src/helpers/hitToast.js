0d0abce56a5ca7ed1b74034fd3964118ca654385
grzafnan <grzafnan@gmail.com>
2023-01-08 00:02:42 +0600

    completed debux

---
+++ src/helpers/hitToast.js
@@ -0,0 +1,17 @@
+import { toast } from 'react-toastify';
+
+const hitToast = (variant, message) => {
+
+    if (['success', 'error'].includes(variant)) {
+        toast.error(message, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+}
+export default hitToast;
